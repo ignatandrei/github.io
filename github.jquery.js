3e54f50e579548ab3cb1fd67528b7ac73f888dde
Andrei Ignat <ignatandrei@yahoo.com>
2015-02-06 08:23:26 +0100

    Update github.jquery.js

--- github.jquery.js
+++ github.jquery.js
@@ -2,7 +2,7 @@
  
 jQuery.githubUser = function(username, callback) {
   //jQuery.getJSON("http://github.com/api/v1/json/" + username + "?callback=?", callback);
-  jQuery.getJSON("https://api.github.com/users/" + username + "?callback=?", callback);
+  jQuery.getJSON("https://api.github.com/users/" + username + "/repos?callback=?", callback);
 }
  
 jQuery.fn.loadRepositores = function(username) {
